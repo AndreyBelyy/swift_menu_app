4f00d0c5caab7568e14446b7ddc51a17029867c7
Andrei Belyi <andreybelyyw@gmail.com>
2024-10-13 19:00:39 -0600

    Initial Commit

---
+++ menu/menuApp.swift
@@ -0,0 +1,17 @@
+//
+//  menuApp.swift
+//  menu
+//
+//  Created by AEGAR on 13/10/24.
+//
+
+import SwiftUI
+
+@main
+struct menuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ menuTests/menuTests.swift
@@ -0,0 +1,17 @@
+//
+//  menuTests.swift
+//  menuTests
+//
+//  Created by AEGAR on 13/10/24.
+//
+
+import Testing
+@testable import menu
+
+struct menuTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
