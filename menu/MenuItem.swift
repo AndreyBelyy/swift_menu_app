91201aa028c4d2f66ad89947f7c32402b6e25978
Andrei Belyi <andreybelyyw@gmail.com>
2024-10-13 20:30:22 -0600

    1st

---
+++ menu/MenuItem.swift
@@ -0,0 +1,14 @@
+//
+//  MenuItem.swift
+//  menu
+//
+//  Created by AEGAR on 13/10/24.
+//
+
+import Foundation
+
+struct MenuItem {
+    var name : String
+    var price : Double
+    var imageName : String
+}
